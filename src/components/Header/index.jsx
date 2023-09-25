0d6e0cfd79e8e193776354d7e98dc90467cc79d1
Luis Polla <luis.polla86@gmail.com>
2023-09-25 17:54:42 -0300

    path 2 25/09

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,33 @@
+import { useNavigate } from 'react-router-dom';
 import styles from './styles.module.css';
 
 export function Header({ title, navbar }) {
+    const navigate = useNavigate();
+
+    const handleNavigation = (path) => {
+        navigate(path);
+    };
+
     return (
         <header className={styles.header}>
             <div className={styles.content}>
-            <img className={styles.logo} src="/logo.svg" alt="Logo" />
+                <img className={styles.logo} src="/logo.svg" alt="Logo" />
                 <h1>{title.toUpperCase()}</h1>
             </div>
             {navbar && (
-                <navbar className={styles.navbar}>
+                <nav className={styles.navbar}>
                     <ul>
-                        <li className={styles.li}><a href="/home" >Início</a></li>
-                        <li className={styles.li}><a href="/historico">Histórico</a></li>
-                        <li className={styles.li}><a href="/">Sair</a></li>
+                        <li className={styles.li} onClick={() => handleNavigation('/home')}>
+                            Início
+                        </li>
+                        <li className={styles.li} onClick={() => handleNavigation('/historico')}>
+                            Histórico
+                        </li>
+                        <li className={styles.li} onClick={() => handleNavigation('/')}>
+                            Sair
+                        </li>
                     </ul>
-                </navbar>
+                </nav>
             )}
         </header>
     );
